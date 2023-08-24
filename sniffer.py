c692fbc37884f917d1d4e0359c9125ac5035fbd8
Chris Lattman <natgeo13@gmail.com>
2023-08-23 23:46:27 -0400

    fix

--- sniffer.py
+++ sniffer.py
@@ -12,13 +12,13 @@ from scapy.all import *
 from parse import parse
 
 def sniff_packet(encrypted: bool):
+    # This gets the OS-specific name of the loopback interface (localhost)
+    ifaces = get_working_ifaces()
+    for iface in ifaces:
+        if iface.ip == "127.0.0.1":
+            loopback_iface = iface.name
+            break
     if not encrypted:
-        # This gets the OS-specific name of the loopback interface (localhost)
-        ifaces = get_working_ifaces()
-        for iface in ifaces:
-            if iface.ip == "127.0.0.1":
-                loopback_iface = iface.name
-                break
         # This filters for TCP segments with a destination port of 5000
         # that include the word "POST" in their decoded data section (aka payload)
         #
@@ -44,7 +44,7 @@ def sniff_packet(encrypted: bool):
         #
         # The request (in raw bytes) is printed out for demonstration
         lfilter = lambda p: TCP in p and Raw in p and p[TCP].dport == 5000
-        pcap = sniff(iface="lo0", lfilter=lfilter, count=1)
+        pcap = sniff(iface=loopback_iface, lfilter=lfilter, count=1)
         request = pcap[0][Raw].load
         print(request)
 
