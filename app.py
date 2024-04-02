e81406fa542aaeaf3c05e82118ef96698f1d3384
Chris Lattman <natgeo13@gmail.com>
2024-04-02 18:36:30 -0400

    Add HTTP Basic authorization and password hashing

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import sys
 from time import time
 from flask import Flask, make_response, render_template, request
+# from werkzeug.datastructures.auth import WWWAuthenticate
 import jwt
+import bcrypt
 
 app = Flask(__name__)
 
@@ -10,19 +12,19 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 logins = {}
 
 
-def create_cookie(username: str, password: str) -> str:
+def create_cookie(username: str, key: bytes) -> str:
     payload = {}
     current_time = int(time())
     payload["exp"] = str(current_time + 86400)
     payload["iat"] = str(current_time)
     payload["sub"] = username
-    return jwt.encode(payload, password, "HS256")
+    return jwt.encode(payload, key, "HS256")
 
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    # If you see a browser message asking you to reconfirm submission, this means
-    # you're resubmitting a POST request
+    # If you see a browser message asking you to confirm form resubmission, this
+    # means you're resending a POST request
     if request.method == "POST":
         username = request.form.get("username")
         password = request.form.get("password")
@@ -30,19 +32,37 @@ def index():
         # print(password)
         # request.files.get("filename").save("captured_file")
 
-        if username not in logins and username is not None and password is not None:
-            logins[username] = password
-            resp = make_response(render_template("success.html", user=username, existing=""))
-            resp.set_cookie("auth_token", create_cookie(username, password), max_age=86400, httponly=True, samesite="Lax")
-            return resp
-        elif username in logins and password == logins[username]:
-            resp = make_response(render_template("success.html", user=username, existing=" back"))
-            resp.set_cookie("auth_token", create_cookie(username, password), max_age=86400, httponly=True, samesite="Lax")
-            return resp
-        else:
-            return make_response(render_template("error.html"), 403)
+        if username is not None and password is not None:
+            if username not in logins:
+                salt = bcrypt.gensalt()
+                hashed_password = bcrypt.hashpw(password.encode(), salt)
+                key = salt + hashed_password
+                logins[username] = key
+                resp = make_response(render_template("success.html", user=username, existing=""))
+                # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie
+                resp.set_cookie("auth_token", create_cookie(username, key), max_age=86400, httponly=True, samesite="Lax")
+                return resp
+            else:
+                key = logins[username]
+                salt = key[:29]
+                hashed_password = key[29:]
+                hashed_provided_password = bcrypt.hashpw(password.encode(), salt)
+                if hashed_password == hashed_provided_password:
+                    resp = make_response(render_template("success.html", user=username, existing=" back"))
+                    resp.set_cookie("auth_token", create_cookie(username, key), max_age=86400, httponly=True, samesite="Lax")
+                    return resp
+        return make_response(render_template("error.html"), 403)
 
     # request.method == "GET"
+
+    # if request.authorization and request.authorization.username == "user" and request.authorization.password == "pass":
+    #     pass
+    # else:
+    #     resp = make_response(render_template("error.html"), 401)
+    #     # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/WWW-Authenticate
+    #     resp.www_authenticate = WWWAuthenticate("basic", {"realm": "Authentication Required"})
+    #     return resp
+
     token = request.cookies.get("auth_token")
     try:
         if token is not None:
@@ -65,7 +85,8 @@ def index():
 @app.route("/logout", methods=["GET"])
 def logout():
     resp = make_response(render_template("index.html"))
-    resp.set_cookie("auth_token", max_age=0)
+    # resp.set_cookie("auth_token", max_age=0)
+    resp.delete_cookie("auth_token")
     return resp
 
 
